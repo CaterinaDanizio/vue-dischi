7618526c785f1f0974443b24d743cde55a80c64b
Caterina <danizio@outlook.it>
2020-12-01 21:33:35 +0100

    Estrazione informazioni da API e impostazione grafica layout

---
+++ js/script.js
@@ -0,0 +1,43 @@
+// Descrizione:
+// Facciamo una chiamata ajax all’API di boolean.
+// L’API ci restituirà una decina di dischi musicali che dovremo stampare a schermo con Vue.
+
+const collection = "https://flynn.boolean.careers/exercises/api/array/music";
+
+var app = new Vue({
+  el: '#app',
+  data: {
+    playlist: [],
+    userSelection: ""
+  },
+
+  mounted: function () {
+      axios.get(collection).then(risposta => {
+      let album = risposta.data.response;
+      this.playlist = album;
+    });
+  },
+
+  methods: {
+    genreSelection: function() {
+         this.playlist.forEach(album => {
+           if (album.genre.toLowerCase().includes(this.userSelection.toLowerCase())) {
+             return album.show = true;
+           }
+           else if (this.userSelection == "All"){
+             return album.show = true;
+           }
+            else {
+             return album.show = false;
+           }
+
+         });
+ }
+
+
+
+
+  }
+
+
+});
